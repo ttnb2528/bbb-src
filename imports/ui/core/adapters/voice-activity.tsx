5a10ff20ba7b214827773b8f93a7af67142b2d8a
ttnb2528 <thientan2812@gmail.com>
2026-03-23 09:44:05 +0700

    feat: add VoiceActivityAdapter to integrate voice activity data with relevant UI hooks.

--- imports/ui/core/adapters/voice-activity.tsx
+++ imports/ui/core/adapters/voice-activity.tsx
@@ -1,32 +1,29 @@
-import { useEffect } from 'react';
-import { useReactiveVar } from '@apollo/client';
-import useVoiceActivity from '/imports/ui/core/hooks/useVoiceActivity';
+import { useEffect } from "react";
+import { useReactiveVar } from "@apollo/client";
+import useVoiceActivity from "/imports/ui/core/hooks/useVoiceActivity";
 import {
   setWhoIsUnmutedLoading,
   useWhoIsUnmutedConsumersCount,
   dispatchWhoIsUnmutedUpdate,
-} from '/imports/ui/core/hooks/useWhoIsUnmuted';
+} from "/imports/ui/core/hooks/useWhoIsUnmuted";
 import {
   setWhoIsTalkingLoading,
   useWhoIsTalkingConsumersCount,
   dispatchWhoIsTalkingUpdate,
-} from '/imports/ui/core/hooks/useWhoIsTalking';
+} from "/imports/ui/core/hooks/useWhoIsTalking";
 import {
   dispatchTalkingUserUpdate,
   setTalkingUserLoading,
   useTalkingUserConsumersCount,
-} from '/imports/ui/core/hooks/useTalkingUsers';
-import ConnectionStatus from '/imports/ui/core/graphql/singletons/connectionStatus';
+} from "/imports/ui/core/hooks/useTalkingUsers";
+import ConnectionStatus from "/imports/ui/core/graphql/singletons/connectionStatus";
 
 const VoiceActivityAdapter = () => {
   const whoIsUnmutedConsumersCount = useWhoIsUnmutedConsumersCount();
   const whoIsTalkingConsumersCount = useWhoIsTalkingConsumersCount();
   const talkingUserConsumersCount = useTalkingUserConsumersCount();
-  const skip = !(
-    whoIsUnmutedConsumersCount
-    + whoIsTalkingConsumersCount
-    + talkingUserConsumersCount > 0
-  );
+  // FORCE SKIP TO FALSE to ensure Voice Activity subscription is always active
+  const skip = false;
   const { data: voiceActivity, loading } = useVoiceActivity(skip);
   const connected = useReactiveVar(ConnectionStatus.getConnectedStatusVar());
 
